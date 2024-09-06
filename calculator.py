5e906b2e5e7d2f2c615a8da566027c5ed65a1acc
RDharshan13122004 <rdharshansrinivas@gmail.com>
2024-09-06 23:40:41 +0530

    Calculator

--- calculator.py
+++ calculator.py
@@ -93,11 +93,11 @@ button_0 = Button(root,text="0",padx=40,pady=20,background='#2e2e2e',fg='#FFFFFF
 
 #operation button
 
-button_addition = Button(root,text="+",padx=40,pady=20,command=addition)
-button_subraction = Button(root,text="-",padx=40,pady=20,command=subraction)
-button_multiplication = Button(root,text="*",padx=40,pady=20,command=multiplication)
-button_division = Button(root,text="/",padx=40,pady=20,command=division)
-button_equalto = Button(root,text="=",padx=39,pady=54,command=equallto)
+button_addition = Button(root,text="+",padx=40,pady=20,background='#2e2e2e',fg='#FFFFFF',command=addition)
+button_subraction = Button(root,text="-",padx=40,pady=20,background='#2e2e2e',fg='#FFFFFF',command=subraction)
+button_multiplication = Button(root,text="*",padx=40,pady=20,background='#2e2e2e',fg='#FFFFFF',command=multiplication)
+button_division = Button(root,text="/",padx=40,pady=20,background='#2e2e2e',fg='#FFFFFF',command=division)
+button_equalto = Button(root,text="=",padx=39,pady=54,background='#2e2e2e',fg='#FFFFFF',command=equallto)
 button_clear = Button(root,text="Clear",padx=79,pady=20,command=clear)
 
 #put the button into the screen in grid format

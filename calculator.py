28a08854b5b2979d64adeb24a719b6f80ea7ae3f
RDharshan13122004 <rdharshansrinivas@gmail.com>
2024-09-06 23:39:46 +0530

    Calculator

--- calculator.py
+++ calculator.py
@@ -79,16 +79,17 @@ def clear():
 
 #defining box
 
-button_1 = Button(root,text="1",padx=40,pady=20,command= lambda: button_click(1))
-button_2 = Button(root,text="2",padx=40,pady=20,command= lambda: button_click(2))
-button_3 = Button(root,text="3",padx=40,pady=20,command= lambda: button_click(3))
-button_4 = Button(root,text="4",padx=40,pady=20,command= lambda: button_click(4))
-button_5 = Button(root,text="5",padx=40,pady=20,command= lambda: button_click(5))
-button_6 = Button(root,text="6",padx=40,pady=20,command= lambda: button_click(6))
-button_7 = Button(root,text="7",padx=40,pady=20,command= lambda: button_click(7))
-button_8 = Button(root,text="8",padx=40,pady=20,command= lambda: button_click(8))
-button_9 = Button(root,text="9",padx=40,pady=20,command= lambda: button_click(9))
-button_0 = Button(root,text="0",padx=40,pady=20,command= lambda: button_click(0))
+button_1 = Button(root,text="1",padx=40,pady=20,background='#2e2e2e',fg='#FFFFFF',command= lambda: button_click(1))
+button_2 = Button(root,text="2",padx=40,pady=20,background='#2e2e2e',fg='#FFFFFF',command= lambda: button_click(2))
+button_3 = Button(root,text="3",padx=40,pady=20,background='#2e2e2e',fg='#FFFFFF',command= lambda: button_click(3))
+button_4 = Button(root,text="4",padx=40,pady=20,background='#2e2e2e',fg='#FFFFFF',command= lambda: button_click(4))
+button_5 = Button(root,text="5",padx=40,pady=20,background='#2e2e2e',fg='#FFFFFF',command= lambda: button_click(5))
+button_5 = Button(root,text="5",padx=40,pady=20,background='#2e2e2e',fg='#FFFFFF',command= lambda: button_click(5))
+button_6 = Button(root,text="6",padx=40,pady=20,background='#2e2e2e',fg='#FFFFFF',command= lambda: button_click(6))
+button_7 = Button(root,text="7",padx=40,pady=20,background='#2e2e2e',fg='#FFFFFF',command= lambda: button_click(7))
+button_8 = Button(root,text="8",padx=40,pady=20,background='#2e2e2e',fg='#FFFFFF',command= lambda: button_click(8))
+button_9 = Button(root,text="9",padx=40,pady=20,background='#2e2e2e',fg='#FFFFFF',command= lambda: button_click(9))
+button_0 = Button(root,text="0",padx=40,pady=20,background='#2e2e2e',fg='#FFFFFF',command= lambda: button_click(0))
 
 #operation button
 
